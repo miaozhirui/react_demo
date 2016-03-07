1a9c2fe9cf6e012a1ae588e005957f1de3f8cd78
miaozhirui <miaozhirui@ziseyiliao.com>
2016-03-07 17:57:40 +0800

    init react+es6+webpack

---
+++ src/component/my-coupons/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+var title = {
+	color:'red'
+}
+
+class MyCoupons extends React.Component{
+	render() {
+		return (
+			<div style={title}>
+				<h1>优惠券列表</h1>
+				<h2>test</h2>
+			</div>
+			
+			)
+	}
+}
+
+
+module.exports = MyCoupons;
